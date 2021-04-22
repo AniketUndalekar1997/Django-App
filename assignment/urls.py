a2fd8e620593eb2d442f781d67626cda61929c5c
AniketUndalekar1997 <80151279+AniketUndalekar1997@users.noreply.github.com>
2021-04-22 15:28:27 +0530

    Update urls.py

--- assignment/urls.py
+++ assignment/urls.py
@@ -23,7 +23,7 @@ urlpatterns = [
     path('', include('myapp.urls')),
 ]
 
- if settings.DEBUG:
-   urlpatterns += staticfiles_urlpatterns()
+if settings.DEBUG:
+    urlpatterns += staticfiles_urlpatterns()
 # if settings.DEBUG:
 #     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
